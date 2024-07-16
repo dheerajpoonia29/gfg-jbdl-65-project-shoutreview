06e235f175becdc4c418438f3774c37b29d3a2d5
Dheeraj Bishnoi <engineerdheerajbishnoi@gmail.com>
2024-07-16 21:37:07 +0530

    add scheduler

--- src/main/java/com/gfg/shoutreview/repository/ReviewRepository.java
+++ src/main/java/com/gfg/shoutreview/repository/ReviewRepository.java
@@ -1,13 +1,19 @@
 package com.gfg.shoutreview.repository;
 
+import com.gfg.shoutreview.domain.Movie;
 import com.gfg.shoutreview.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review,Long> {
 
     @Query(value = "select avg(rating) from review_table where movie_id=?",nativeQuery = true)
     Double getReviewAverage(Long id);
+
+    @Query(value = "SELECT * FROM review_table WHERE DATE_FORMAT(updated_date, '%d-%m-%Y') = ?", nativeQuery = true)
+    List<Review> findByUpdatedToday(String formattedDate);
 }
--- src/main/java/com/gfg/shoutreview/service/ReviewService.java
+++ src/main/java/com/gfg/shoutreview/service/ReviewService.java
@@ -6,9 +6,15 @@ import com.gfg.shoutreview.repository.MovieRepository;
 import com.gfg.shoutreview.repository.ReviewRepository;
 import com.gfg.shoutreview.service.response.ReviewResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class ReviewService {
@@ -22,14 +28,23 @@ public class ReviewService {
     public void addReview(Review review) {
         Movie movie=movieRepository.findById(review.getMovie().getId()).orElse(null);
         reviewRepository.save(review);
-        //need to optimized
-        //exception handling.
-        if(movie!=null) {
+    }
+
+    @Scheduled(fixedRate = 20 * 1000) // 10 sec in milliseconds
+    private void updateAvgOfMovie() {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = today.format(formatter);
+
+        List<Review> reviewList = reviewRepository.findByUpdatedToday(formattedDate);
+
+        reviewList.stream().forEach(review -> {
+            Movie movie = review.getMovie();
+            // dheeraj: update the average review after each 4 hour, not every time this function call
             Double average = reviewRepository.getReviewAverage(movie.getId());
             movie.setRating(average);
             movieRepository.save(movie);
-        }
-
+        });
     }
 
     public ReviewResponse getReviewById(Long reviewId) {
